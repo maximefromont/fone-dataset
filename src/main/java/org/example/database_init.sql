7b4f251c561ab7ac5017bb46d9d0c56a4bcbd246
DimitriDR <dimitridroeck@gmail.com>
2024-01-11 22:16:58 +0100

    Updating the SQL to use auto-increment IDs and adding nationality to constructors.

--- src/main/java/org/example/database_init.sql
+++ src/main/java/org/example/database_init.sql
@@ -1,15 +1,16 @@
-CREATE TABLE Driver(
-                       id_driver INT,
-                       lastname_driver VARCHAR(50) NOT NULL,
-                       firstname_driver VARCHAR(50) NOT NULL,
-                       PRIMARY KEY(id_driver)
+CREATE TABLE Driver
+(
+    id_driver        SERIAL PRIMARY KEY,
+    lastname_driver  VARCHAR(50) NOT NULL,
+    firstname_driver VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Constructor(
-                            id_constructor INT,
-                            name_constructor VARCHAR(250) NOT NULL,
-                            PRIMARY KEY(id_constructor),
-                            UNIQUE(name_constructor)
+CREATE TABLE Constructor
+(
+    id_constructor          SERIAL PRIMARY KEY,
+    name_constructor        VARCHAR(250) NOT NULL,
+    nationality_constructor VARCHAR(250) NOT NULL,
+    UNIQUE (name_constructor)
 );
 
 /*
@@ -21,20 +22,20 @@ CREATE TABLE Standing(
 );
 */
 
-CREATE TABLE Race(
-                     id_race INT,
-                     location_race VARCHAR(250) NOT NULL,
-                     PRIMARY KEY(id_race),
-                     UNIQUE(location_race)
+CREATE TABLE Race
+(
+    id_race       SERIAL PRIMARY KEY,
+    location_race VARCHAR(250) NOT NULL UNIQUE
 );
 
-CREATE TABLE Teamed(
-                       id_driver INT,
-                       id_constructor INT,
-                       year_teamed VARCHAR(50) NOT NULL,
-                       PRIMARY KEY(id_driver, id_constructor),
-                       FOREIGN KEY(id_driver) REFERENCES Driver(id_driver),
-                       FOREIGN KEY(id_constructor) REFERENCES Constructor(id_constructor)
+CREATE TABLE Teamed
+(
+    id_driver      INT,
+    id_constructor INT,
+    year_teamed    VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_driver, id_constructor),
+    FOREIGN KEY (id_driver) REFERENCES Driver (id_driver),
+    FOREIGN KEY (id_constructor) REFERENCES Constructor (id_constructor)
 );
 
 /*
@@ -47,12 +48,13 @@ CREATE TABLE Stood(
 );
 */
 
-CREATE TABLE Earned(
-                       id_driver INT,
-                       id_race INT,
-                       points_earned INT NOT NULL,
-                       year_earned VARCHAR(50) NOT NULL,
-                       PRIMARY KEY(id_driver, id_race),
-                       FOREIGN KEY(id_driver) REFERENCES Driver(id_driver),
-                       FOREIGN KEY(id_race) REFERENCES Race(id_race)
+CREATE TABLE Earned
+(
+    id_driver     INT,
+    id_race       INT,
+    points_earned INT         NOT NULL,
+    year_earned   VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_driver, id_race),
+    FOREIGN KEY (id_driver) REFERENCES Driver (id_driver),
+    FOREIGN KEY (id_race) REFERENCES Race (id_race)
 );
